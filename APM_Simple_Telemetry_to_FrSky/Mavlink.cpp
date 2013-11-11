b17db2a94f05740482b9593d28a3a5852f0dbb83
Nils Högberg <nils.hogberg@gmail.com>
2013-11-11 22:54:53 +0100

    Added link to ArduCam_OSD in Mavlink.cpp

--- APM_Simple_Telemetry_to_FrSky/Mavlink.cpp
+++ APM_Simple_Telemetry_to_FrSky/Mavlink.cpp
@@ -2,6 +2,8 @@
 	@author 	Nils Högberg
 	@contact 	nils.hogberg@gmail.com
 
+	Original code from https://code.google.com/p/arducam-osd/wiki/arducam_osd
+
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
